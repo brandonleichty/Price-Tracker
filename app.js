2c83a562df5fc3f4d61f27cf6607644319140312
Brandon Leichty <brandonleichty@Brandons-MBP.domain>
2017-12-27 19:31:58 -0600

    added lots of fun stuff (compares the initial price to the current price, and sends appropriate text message)

--- app.js
+++ app.js
@@ -12,70 +12,92 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 // require the Twilio module and create a REST client
 const client = require('twilio')(accountSid, authToken);
 
-// client.messages.create(
-//   {
-//     to: process.env.MY_PHONE_NUMBER,
-//     from: process.env.TWILIO_PHONE_NUMBER,
-//     body: "This is a test!",
-//   },
-//   (err, message) => {
-//     console.log(message.sid);
-//     console.log('It worked!');
-//   }
-// );
+function sendTextMessage(productName, previousPrice, currentPrice) {
+    client.messages.create({
+            to: process.env.MY_PHONE_NUMBER,
+            from: process.env.TWILIO_PHONE_NUMBER,
+            body: `${productName} has changed from ${previousPrice} to ${currentPrice}.`,
+        },
+        (err, message) => {
+            console.log(message.sid);
+            console.log('Message sent!'); //This is just for fun
+        }
+    );
+}
 
-const endPoint = 'https://www.target.com/p/brass-library-task-lamp-hearth-hand-153-with-magnolia/-/A-52677414';
+
+function sendInitialTextMessage(name, price) {
+    client.messages.create({
+            to: process.env.MY_PHONE_NUMBER,
+            from: process.env.TWILIO_PHONE_NUMBER,
+            body: `${name} is currently ${price}. We'll update you if anything changes. -EVE`,
+        },
+        (err, message) => {
+            console.log(message.sid);
+            console.log('Message sent!'); //This is just for fun
+        }
+    );
+}
+
+
+
+let initialProductPrice = '$10.99';
+
+const endPoint = 'https://www.target.com/p/stoneware-mug-14oz-hearth-hand-153-with-magnolia/-/A-52991410';
 
 
 const targetPriceSelector = '#js-product-sr-id > div:nth-child(1) > span:nth-child(1)';
- 
-const getPrice = async () => {
-    const browser = await puppeteer.launch({headless: false});
+const targetProductNameSelector = '.title-product > span:nth-child(1)';
+
+const scrapeProductInfo = async() => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
     const page = await browser.newPage();
-  
+
     await page.goto(endPoint);
     await page.waitFor(targetPriceSelector);
-  
+
+    // get product price
     const price = await page.evaluate((targetPriceSelector) => {
         const priceSpan = document.querySelector(targetPriceSelector);
-        // returned data must be valid JSON        
-      return priceSpan.innerHTML;
+        return priceSpan.innerHTML.trim();
     }, targetPriceSelector);
-  
+
+    // get product name
+    const name = await page.evaluate((targetProductNameSelector) => {
+        const nameSpan = document.querySelector(targetProductNameSelector);
+        return nameSpan.innerHTML;
+    }, targetProductNameSelector);
+
+
     await browser.close();
-    return price;
-  };
+    return {
+        price: price,
+        name: name
+    }; //return object with price and name of product
+};
 
-  const logPrice = async () => {
-    const newPrice = await getPrice();
-    console.log(newPrice);
-    console.log('💩');
-  }
-
-logPrice();
-
-
-
-//This is where I place the link/url to the product I'd like to track
-// const endPoint = 'https://www.target.com/p/plaid-tree-skirt-green-hearth-hand-153-with-magnolia/-/A-52586856#lnk=sametab';
-
-
-
-// fetch(endPoint)
-//     .then(function(res) {
-//         return res.text();
-//     }).then(function(body) {
-//         loadCheerio(body);
-//         // console.log(body);
-//     });
-
-
-// fetch(endPoint)
-//     .then(data => console.log(data));
-
-// function loadCheerio(htmlToLoad) {
-//   const $ = cheerio.load(htmlToLoad);
-//   const title = $('#js-pdpChatURL').text();
-//   console.log(title);
-//   console.log('This worked');
+// const logPrice = async() => {
+//     const newPrice = await getPrice();
+//     console.log(newPrice);
+//     console.log('💩');
 // }
+
+const checkForPriceChanges = async() => {
+    const product = await scrapeProductInfo();
+
+    if(initialProductPrice === null){
+        initialProductPrice = product.price;
+        sendInitialTextMessage(product.name, product.price);
+    } else {
+        sendTextMessage(product.name, initialProductPrice, product.price);
+        console.log(`The name of the product is ${product.name}, and the price is ${product.price}.`);
+    }
+
+    console.log(`${product.price}💩💩💩`);
+    console.log(`${initialProductPrice}`);
+
+};
+
+checkForPriceChanges();
